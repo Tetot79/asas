1c2cb2a4300ed2b819714f5495673c10bb36c928
Tetot79 <tetost79@gmail.com>
2025-07-06 14:03:55 +0200

    M

--- js/file.js
+++ js/file.js
@@ -504,7 +504,7 @@ let loginEmail = document.querySelector(".login-email");
           !window.localStorage.getItem("loginEmail") ||
           !window.localStorage.getItem("loginPassword")
         ) {
-          window.open("Login.html#login");
+          window.open("../Login.html","_blank");
         }
       });
     });
@@ -516,7 +516,7 @@ let loginEmail = document.querySelector(".login-email");
           !window.localStorage.getItem("loginEmail") ||
           !window.localStorage.getItem("loginPassword")
         ) {
-          window.open("Login.html#login");
+          window.open("../Login.html","_blank");
         }
       });
     });
